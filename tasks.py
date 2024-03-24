2aeaf7f67b655c22779f690ffa880273b4506566
Shakil_xBCC <shakilmrf8@gmail.com>
2024-03-24 12:23:26 +0600

    tasks.py => contains source code for queing email msg (rq)

--- tasks.py
+++ tasks.py
@@ -14,15 +14,17 @@ def send_user_registration_email(subject, message, user_email):
 
     service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-    emailMsg = f'Welcome to our platform, {user}!'
+    #emailMsg = f'Welcome to our platform, {user}!'
     mimeMessage = MIMEMultipart()
     mimeMessage['to'] = user_email
     mimeMessage['subject'] = 'Registration Confirmation Email'
-    mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+    mimeMessage.attach(MIMEText(message, 'plain'))
     raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
-
-    message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
-    print(message)
+    try:
+        message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
+        print("Email sent successfully:", message)
+    except Exception as e:
+        print("An error occurred while sending the email:", e)
 
 # Example usage:
 # send_user_registration_email('Welcome!', 'example@gmail.com', 'John Doe')

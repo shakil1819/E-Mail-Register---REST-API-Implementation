09edeb8f2148534445b0569aad4c1dd222164499
Shakil_xBCC <shakilmrf8@gmail.com>
2024-03-24 16:17:20 +0600

    user.py => contains all endpoints
    - POST/ register
    - DELETE/ deleteall
    - GET/ user<user:id>

--- resources/user.py
+++ resources/user.py
@@ -3,13 +3,6 @@ import redis
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-# from flask_jwt_extended import (
-#     create_access_token,
-#     create_refresh_token,
-#     get_jwt_identity,
-#     jwt_required,
-#     get_jwt,
-# )
 from rq import Queue
 from flask import current_app,jsonify
 from sqlalchemy import or_
@@ -24,7 +17,7 @@ from tasks import send_user_registration_email
 blp = Blueprint("Users", "users", description="Operations on users")
 connection = redis.from_url(
     os.getenv("REDIS_URL")
-)  # Get this from Render.com or run in Docker
+)
 queue = Queue("emails", connection=connection)
 
 
@@ -32,12 +25,7 @@ queue = Queue("emails", connection=connection)
 class UserRegister(MethodView):
     @blp.arguments(UserSchema)
     def post(self, user_data):
-        if UserModel.query.filter(
-            or_(
-                UserModel.username == user_data["username"],
-                UserModel.email == user_data["email"],
-            )
-        ).first():
+        if UserModel.query.filter(UserModel.email == user_data["email"]).first():
             abort(409, message="A user with that username or email already exists.")
 
         user = UserModel(
@@ -51,8 +39,7 @@ class UserRegister(MethodView):
         subject = "Welcome!Your registration was successful"
         message = f"Welcome to our platform, {user.username}!"
 
-        current_app.queue.enqueue(send_user_registration_email, subject, message, user_data["email"])
-
+        current_app.queue.enqueue(send_user_registration_email,subject, message, user.email)
         return {"message": "User created successfully."}, 201
 
 @blp.route("/user")
@@ -73,3 +60,14 @@ class User(MethodView):
         db.session.delete(user)
         db.session.commit()
         return {"message": "User deleted."}, 200
+
+@blp.route("/deleteall")
+class DeleteAllUsers(MethodView):
+    def delete(self):
+        try:
+            db.session.query(UserModel).delete()
+            db.session.commit()
+            return {"message": "All data deleted successfully."}, 200
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"An error occurred: {e}"}, 500

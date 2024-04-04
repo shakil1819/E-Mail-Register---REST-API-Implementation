0f3f63e2be3a9666b14780ff56be8c0a30a2d70d
Shakil_xBCC <shakilmrf8@gmail.com>
2024-04-04 18:53:30 +0600

    Solved the condition where Database failure error is fixed
    - Added datetime and retry function in for loop with intervals
    - Upon  Database connection failure, it rollsback and api response is " Database error occured"

--- resources/user.py
+++ resources/user.py
@@ -15,6 +15,7 @@ from schemas import UserSchema#, UserRegisterSchema
 from tasks import send_user_registration_email
 from validate_email import validate_email
 from dotenv import load_dotenv
+from datetime import timedelta
 load_dotenv()
 
 blp = Blueprint("Users", "users", description="Operations on users")
@@ -25,7 +26,6 @@ connection=Redis(host='redis',port=6379)
 # connection=Redis(host='rq_worker',port=6379)
 queue = Queue("emails", connection=connection)
 
-
 @blp.route("/register")
 class UserRegister(MethodView):
     @blp.arguments(UserSchema)
@@ -34,7 +34,6 @@ class UserRegister(MethodView):
             abort(409, message="A user with that username or email already exists.")
 
         is_valid = validate_email(user_data["email"])
-        #print(is_valid) 
         if not is_valid:
             abort(400, message="Invalid email address.")
 
@@ -43,14 +42,26 @@ class UserRegister(MethodView):
             email=user_data["email"],
             password=pbkdf2_sha256.hash(user_data["password"]),
         )
-        db.session.add(user)
-        db.session.commit()
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"Database error occurred: {str(e)}"}, 500
 
         subject = "Welcome!Your registration was successful"
         message = f"Welcome to our platform, {user.username}!"
 
-        current_app.queue.enqueue(send_user_registration_email,subject, message, user.email)
+        for i in range(3):
+            delay=timedelta(minutes=30 * (i + 1))
+            current_app.queue.enqueue_in(send_user_registration_email,subject, message, user.email, retry=i)
+
+        # Store the email in Redis
+        # connection.set(user.email, 'pending')
+
         return {"message": "User created successfully."}, 201
+
+
 
 @blp.route("/user")
 class StoreList(MethodView):
